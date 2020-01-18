c2ddc6b8ca4b6a40affeb259567882df28e6fcfe
Tran Duy Nghia <tdnghia98@gmail.com>
2020-01-18 11:51:52 +0100

    refactor: bottom_button

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,4 @@
-import 'package:bmi_calculator/result_page.dart';
+import 'bottom_button.dart';
 import 'package:flutter/material.dart';
 
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -199,34 +199,6 @@ class _InputPageState extends State<InputPage> {
               BottomButton(),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class BottomButton extends StatelessWidget {
-  const BottomButton({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 20.0),
-      // Stretch width to full screen
-      width: double.infinity,
-      height: kBottomContainerHeight,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20.0),
-          topRight: Radius.circular(20.0),
-        ),
-        color: kRedPinkAccentColor,
-      ),
-      child: Center(
-        child: Text(
-          'CALCULATE',
         ),
       ),
     );
--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,5 +1,5 @@
 import 'package:bmi_calculator/constants.dart';
-import 'package:bmi_calculator/input_page.dart';
+import 'bottom_button.dart';
 
 import './reusable_card.dart';
 import 'package:flutter/material.dart';
